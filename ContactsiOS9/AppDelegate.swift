3abc894ed1a9f8152de01c54ceeedfe3ad61d426
cwnidog <cwnidog@comcast.net>
2015-10-19 16:23:27 -0700

    Initial files

---
+++ ContactsiOS9/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ContactsiOS9
+//
+//  Created by AppsFoundation on 11/10/15.
+//  Copyright © 2015 AppsFoundation. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
---
+++ ContactsiOS9/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  ContactsiOS9
+//
+//  Created by AppsFoundation on 11/10/15.
+//  Copyright © 2015 AppsFoundation. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//MARK: - User Actions
+    
+    @IBAction func textFieldValueChanged(sender: AnyObject) {
+        
+    }
+
+}
